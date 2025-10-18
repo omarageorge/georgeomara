ed73451f10548fe0e530ce63c52c1cb73f68e5ab
George Omara <georgeomara8@gmail.com>
2025-10-18 11:16:17 +0300

    feat(footer): create footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className='w-full py-4'>
+    <p className='font-base text-center'>
+      Copyright &copy; George Omara | {new Date().getFullYear()}
+    </p>
+  </footer>
+);
+
+export default Footer;
