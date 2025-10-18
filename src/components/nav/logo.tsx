d7aec6d4c8746b9327cff568242aade917b5858f
George Omara <georgeomara8@gmail.com>
2025-10-18 11:14:28 +0300

    refactor(logo): change logo appearance

--- src/components/nav/logo.tsx
+++ src/components/nav/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Logo = () => (
   <div className=''>
     <Link href='/' className='btn btn-ghost font-semibold text-xl overlay-pane'>
-      George &lt;&#47;&gt;
+      &lt;George &#47;&gt;
     </Link>
   </div>
 );
