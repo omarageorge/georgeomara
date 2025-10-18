8c3d1b8980d9674df1bfb92d7a8167595f8b06d5
George Omara <georgeomara8@gmail.com>
2025-10-18 12:41:41 +0300

    feat(components): create components for displaying a project and a list of projects

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -1,31 +1,27 @@
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
-type Props = {
+export interface IProject {
   url: string;
   image: StaticImageData;
   title: string;
   description: string;
   stack: string[];
-};
+}
 
-const Project = ({ url, image, title, description, stack }: Props) => (
-  <Link
-    href={url}
-    target='_blank'
-    className='block w-full border-4 rounded-xl p-4 hover:bg-gray-900 hover:scale-105 shadow transition duration-500 ease-in-out'
-  >
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+const Project = ({ url, image, title, description, stack }: IProject) => (
+  <Link href={url} target='_blank' className='block w-full p-0 md:p-2'>
+    <div className='grid grid-cols-1 gap-2'>
       <Image
         src={image}
         alt=''
-        className='aspect-16/9  bg-slate-800 rounded-xl overflow-hidden '
+        className='aspect-16/9 hover:scale-105 rounded-xl overflow-hidden transition-all duration-500 ease-in-out'
       />
 
-      <div className='flex flex-col items-start justify-between p-0 md:p-4 rounded-xl'>
+      <div className='flex flex-col items-start justify-between p-2 gap-y-2 rounded-xl'>
         <h3 className='font-bold text-lg underline'>{title}</h3>
         <p className='text-md line-clamp-4'>{description}</p>
-        <p className='font-semibold'>{stack.join(', ')}</p>
+        <p className='font-semibold text-secondary'>{stack.join(', ')}</p>
       </div>
     </div>
   </Link>
--- src/components/projects/projects-list.tsx
+++ src/components/projects/projects-list.tsx
@@ -1,3 +1,12 @@
-const ProjectsList = () => <div className=''></div>;
+import Project from '@/components/projects/project';
+import { projects } from '@/lib/data';
+
+const ProjectsList = () => (
+  <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+    {projects.map((project) => (
+      <Project key={project.title} {...project} />
+    ))}
+  </div>
+);
 
 export default ProjectsList;
