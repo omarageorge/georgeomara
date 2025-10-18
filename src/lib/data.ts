6e1db0723766cee34b32733e9ae1b42a92e3db0a
George Omara <georgeomara8@gmail.com>
2025-10-18 11:20:01 +0300

    refactor(data): modify navLinks

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,8 @@
 import { NavLink } from '@/lib/definitions';
 
 export const navLinks: NavLink[] = [
-  { href: '/', label: 'Home' },
-  { href: '/#about', label: 'About' },
-  { href: '/#skills', label: 'Skills' },
-  { href: '/#projects', label: 'Projects' },
-  { href: '/#contact', label: 'Contact' },
+  { href: '#intro', label: 'Home' },
+  { href: '#about', label: 'About' },
+  { href: '#projects', label: 'Projects' },
+  { href: '#contact', label: 'Contact' },
 ];
