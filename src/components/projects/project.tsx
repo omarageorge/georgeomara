94b4ed799df2106a0c333fd179435c924760cbd7
George Omara <georgeomara8@gmail.com>
2025-10-18 12:59:30 +0300

    refactor(button): add rounded corners to outer most component, and a background color on hover

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -10,7 +10,11 @@ export interface IProject {
 }
 
 const Project = ({ url, image, title, description, stack }: IProject) => (
-  <Link href={url} target='_blank' className='block w-full p-0 md:p-2'>
+  <Link
+    href={url}
+    target='_blank'
+    className='block w-full p-0 md:p-2 rounded-xl overflow-hidden hover:bg-base-300'
+  >
     <div className='grid grid-cols-1 gap-2'>
       <Image
         src={image}
@@ -18,7 +22,7 @@ const Project = ({ url, image, title, description, stack }: IProject) => (
         className='aspect-16/9 hover:scale-105 rounded-xl overflow-hidden transition-all duration-500 ease-in-out'
       />
 
-      <div className='flex flex-col items-start justify-between p-2 gap-y-2 rounded-xl'>
+      <div className='flex flex-col items-start justify-between p-2 gap-y-2'>
         <h3 className='font-bold text-lg underline'>{title}</h3>
         <p className='text-md line-clamp-4'>{description}</p>
         <p className='font-semibold text-secondary'>{stack.join(', ')}</p>
