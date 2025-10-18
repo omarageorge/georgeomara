34e3e18de5cfad25222ccdccc6907b03d8ff940f
George Omara <georgeomara8@gmail.com>
2025-10-18 11:20:38 +0300

    feat(index): add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,27 @@
+import me from '@/assets/me.png';
 import Button from '@/components/button';
+import SectionHeading from '@/components/section-heading';
+import SectionLabel from '@/components/section-label';
+import TechStackList from '@/components/tech/tech-stack-list';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <header className='w-full h-[calc(100vh-4rem)] flex flex-col items-center justify-center'>
+      <header
+        id='intro'
+        className='w-full h-[calc(70vh-4rem)] md:h-[calc(100vh-4rem)] mt-16 flex flex-col items-center justify-center'
+      >
         <div className='space-y-4 md:space-y-6 text-center md:text-left'>
-          <h2 className=' font-semibold text-3xl lg:text-5xl'>
-            Hi! I&apos;m{' '}
-            <span className='font-bold text-orange-600'>George</span>
+          <h2 className='text-medium'>
+            Hi! I&apos;m <span className='font-bold text-primary'>George</span>
           </h2>
-          <h3 className='font-bold text-4xl lg:text-6xl'>
-            Full-Stack Software Developer
-          </h3>
-          <h3 className='font-normal text-base lg:text-lg max-w-sm md:max-w-md lg:max-w-xl mx-auto md:ml-0'>
-            Passionate about building high-performance web and mobile apps that
-            solve your toughest challenges and boost your business growth.
+          <h3 className='text-large'>Full-Stack Software Developer</h3>
+          <h3 className='text-body max-w-sm md:max-w-md lg:max-w-xl mx-auto md:ml-0'>
+            Passionate about building high-performance <strong>Web</strong> and{' '}
+            <strong>Mobile</strong> apps that solve your toughest challenges and
+            boost business growth.
           </h3>
           <h4 className='w-fit flex gap-x-1 text-lg mx-auto md:ml-0'>
             <span>
@@ -34,9 +41,59 @@ export default function Home() {
             <span>Gulu, Uganda</span>
           </h4>
 
-          <Button label='Get to know me' primary />
+          <Link href='#about'>
+            <Button
+              label='Get to know me'
+              primary
+              className='md:btn-lg rounded-xl'
+            />
+          </Link>
         </div>
       </header>
+
+      <main className='w-full space-y-8 md:space-y-16 mb-8 md:mb-16'>
+        <section id='about' className='w-full space-y-16'>
+          <div className='flex flex-col sm:flex-row gap-6 md:gap-8'>
+            <div className='flex-1 md:flex-none w-full sm:w-56 md:w-80 justify-items-center content-center'>
+              <Image
+                src={me}
+                priority
+                placeholder='blur'
+                alt='A photo of Omara George Boniface'
+                className='aspect-square sm:aspect-9/12 object-cover object-top overflow-hidden rounded-xl shadow-lg'
+              />
+            </div>
+
+            <div className='flex-1 space-y-4 text-left content-center'>
+              <SectionHeading label='about me' />
+
+              <div className='text-body space-y-4'>
+                <p>
+                  Experienced software engineer with over four years of
+                  experience developing and optimizing solid front-end and
+                  server-side applications.
+                </p>
+
+                <p>
+                  Expertise in designing and implementing RESTful
+                  APIs, architecting intricate database systems, and integrating
+                  with cloud platforms. competent in performance tuning to
+                  ensure the scalability and reliability of overall systems.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div id='stack' className='space-y-10'>
+            <SectionLabel label='technologies I enjoy working with' />
+            <TechStackList />
+          </div>
+        </section>
+
+        <section id='projects' className='space-y-10 md:space-y-16'>
+          <SectionHeading label='some of my work' left />
+        </section>
+      </main>
     </>
   );
 }
