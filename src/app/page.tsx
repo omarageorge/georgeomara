9da48850b2c067e254ae8d85382898678b049ab4
George Omara <georgeomara8@gmail.com>
2025-10-21 17:59:08 +0300

    remove blank target from resume download link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,6 @@ export default function Home() {
           <Link
             href='/my-resume.pdf'
             download='Omara_George_Boniface_Professional_Resume.pdf'
-            target='_blank'
             className='btn btn-primary md:btn-lg rounded-xl animate-pulse'
           >
             <svg
@@ -57,7 +56,7 @@ export default function Home() {
               <path d='M12 1.5a.75.75 0 0 1 .75.75V7.5h-1.5V2.25A.75.75 0 0 1 12 1.5ZM11.25 7.5v5.69l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z' />
             </svg>
 
-            <span>Download Resume</span>
+            <span>Download my resume</span>
           </Link>
         </div>
       </header>
@@ -116,7 +115,7 @@ export default function Home() {
 
         <section id='contact'>
           <div className='w-full max-w-xl mx-auto space-y-6'>
-            <SectionLabel label="Let's connect today!" />
+            <SectionLabel label="Let's connect!" />
 
             <p className='w-full max-w-md mx-auto text-center'>
               Thanks for dropping in! Got questions or just want to connect?
